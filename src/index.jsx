2e16ae37d0bc2b79c38e74819642ff5506f9fd1b
Rivonny Wulandari <rivonnywulandari19@gmail.com>
2025-07-06 15:29:48 +0700

    update package.json

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import OrderApp from './components/order/OrderApp';
-import App from './App'; 
 import './styles/style.css';
 
 
 const root = createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(<OrderApp />);
 
 
