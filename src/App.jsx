e32986e175640486bd850e2bbf03d8e7d42b2e0c
Rivonny Wulandari <rivonnywulandari19@gmail.com>
2025-07-06 15:24:15 +0700

    router update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-// App.jsx
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import OrderApp from './components/order/OrderApp';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/mycoffeeshop">
       <Routes>
         <Route path="/" element={<OrderApp />} />
       </Routes>
